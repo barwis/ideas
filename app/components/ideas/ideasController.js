8e33f7d8a3629fed5e9cb771344ed701991d618a
barwis <lordzix@gmail.com>
2016-05-18 23:42:44 +0100

    status info. focus handling

--- app/components/ideas/ideasController.js
+++ app/components/ideas/ideasController.js
@@ -1,9 +1,9 @@
 angular
     .module('ideas', ['firebase'])
     .constant('FIREBASE_URI','https://fiery-torch-3896.firebaseio.com/')
-    .controller('IdeasController', ['$scope', 'FIREBASE_URI', '$firebaseArray', function($scope, FIREBASE_URI, $firebaseArray) {
+    .controller('IdeasController', ['$scope','FIREBASE_URI', '$firebaseArray', function($scope,  FIREBASE_URI, $firebaseArray) {
         $scope.title = "Ideas..."
-        $scope.myIdeas = ["asd"];
+        $scope.myIdeas = [];
         
         var ref = new Firebase(FIREBASE_URI);
         
@@ -13,10 +13,14 @@ angular
         $scope.myIdeas = list;
 
 
-        $scope.save = function(id) {
+        $scope.save = function(e, id) {
             console.log('saving ', id,  '...');
+            console.log(e.target.parentNode.getElementsByClassName('status'));
+            var $t = $(e.target.parentNode.getElementsByClassName('status'));
+            $t.text('saving...').css('opacity', 1);
             list.$save(id).then(function(ref) {
                 console.log('saved!')
+                $t.text('done!').delay(200).animate({ opacity: 0}, 500, function() {})
             })
         }
 
@@ -27,5 +31,26 @@ angular
             })
         }
 
-    }]);
+        $scope.add = function() {
+            console.log('adding new entry...');
+            var newIdea = {
+                'title' : '',
+                'message' : '',
+                'created' : new Date().toISOString().slice(0, 10)
+            }
+            $scope.myIdeas.$add(newIdea);
+        }
+
+    }])
+    .directive('setFocus', function(){
+      return{
+          scope: {setFocus: '='},
+          link: function(scope, element){
+             if(scope.setFocus) {
+                console.log('focus here!', element[0].childNodes[3])
+                element[0].childNodes[3].focus();             
+                }
+          }
+      };
+    });
     
