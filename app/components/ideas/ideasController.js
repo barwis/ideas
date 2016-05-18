c9e7862962e738e8f3ca2c6aa099891932e575d8
Bartosz Wisniewski <bartosz.wisniewski@virginholidays.com>
2016-05-18 18:07:16 +0100

    new fonts; added firebase binding

--- app/components/ideas/ideasController.js
+++ app/components/ideas/ideasController.js
@@ -1,21 +1,31 @@
 angular
     .module('ideas', ['firebase'])
-    .constant('FIREBASE_URI','https://fiery-torch-3896.firebaseio.com')
-    .controller('IdeasController', ['$scope', 'FIREBASE_URI', '$firebaseObject', function($scope, FIREBASE_URI, $firebaseObject) {
-        // $scope.items = IdeasService.getIdeas();
-             
+    .constant('FIREBASE_URI','https://fiery-torch-3896.firebaseio.com/')
+    .controller('IdeasController', ['$scope', 'FIREBASE_URI', '$firebaseArray', function($scope, FIREBASE_URI, $firebaseArray) {
+        $scope.title = "title"
+        $scope.myIdeas = ["asd"];
+        
         var ref = new Firebase(FIREBASE_URI);
-        var ideas = $firebaseObject(ref);
         
-        
-        ideas.$bindTo($scope, 'items');
-        
-        ideas.$loaded().then(function() {
-            console.log('ideas loaded!');
-            // console.log(ideas)
-            // $scope.ideas['id_4'] = {'title' : 'this is title 4', 'message' : 'this is message 4'}; 
-        });
-        
-        
+        var list = $firebaseArray(ref);
+
+
+        $scope.myIdeas = list;
+
+
+        $scope.save = function(id) {
+            console.log('saving ', id,  '...');
+            list.$save(id).then(function(ref) {
+                console.log('saved!')
+            })
+        }
+
+        $scope.remove = function(id) {
+            console.log('removing ', id, '...');
+            list.$remove(id).then(function(ref) {
+                console.log('removed!', ref);
+            })
+        }
+
     }]);
     
